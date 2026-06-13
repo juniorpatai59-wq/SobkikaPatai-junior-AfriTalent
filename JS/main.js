baa1683a70922631476cdee1ff1a98c237714d46
Sobkika Patai juniior <juniorpatai59@gmail.com>
2026-06-13 15:55:34 +0000

    JS : dark mode avec localstorage, navbar dynamique au scroll

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,45 @@
-  
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('mainNavbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+const btnDarkMode= document.getElementById('btn-darkmode');
+
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+    if (btnDarkMode) btnDarkMode.innerHTML = '<i class="bi bi-sun-fill"></i>';
+} 
+
+if (btnDarkMode) {
+    btnDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('theme', 'dark');
+            btnDarkMode.innerHTML = '<i class="bi bi-sun-fill"></i>';
+        } else {
+            localStorage.setItem('theme', 'light');
+            btnDarkMode.innerHTML = '<i class="bi bi-moon-fill"></i>';
+        }
+    });
+}
+
+const btnTop = document.getElementById('btn-top');
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+        btnTop.style.display = 'flex';
+    } else {
+        btnTop.style.display = 'none';
+    }
+});
+
+if (btnTop) {
+    btnTop.addEventListener('click', function() {
+        window.scrollTo({ top: 0, behavior: 'smooth'});
+    });
+}
